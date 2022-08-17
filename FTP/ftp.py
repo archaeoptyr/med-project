8a8fd9b019ded692616e0e0c04b654d95dc609ab
Rskas <110549485+Rskas@users.noreply.github.com>
2022-08-18 00:12:30 +0100

    Persistent Connection
    - User does not have to enter the FTP details every time
    
    - The schedule arg reformats the date and time

--- FTP/ftp.py
+++ FTP/ftp.py
@@ -1,80 +1,109 @@
+import argparse
+from genericpath import isfile
+import pickle
 from ftplib import FTP
 import time
+import os
+import datetime
 
-ftp = ""
-hostname = input("Enter Hostname: ")
-username = input("Enter Username: ")
-password = input("Enter Password: ")
+ftp = None
 
 def connect_ftp(hostname, username, password):
     try:
         global ftp
+        
+        #Creates a socket to connect to FTP server
         ftp = FTP(hostname)
         ftp.login(username, password)
+
+        print("Connected")
+        keep_conn(args)
+
     
-        print("Connected to: " + hostname)
-        
     except:
         print("Unable to connect")
         print("\nClosing in 5 seconds")
         time.sleep(5)
         quit()
+    
 
-def list_files():
-    print()
-    ftp.retrlines('LIST')
+def keep_conn(cred):
+    #creates pickle file to store ftp creds
+    file_conn = open('ftp_conn.obj', 'wb')
+    pickle.dump(cred, file_conn)
 
+def conn_exists():
+    #Checks if the pickle file exists 
+    if os.path.isfile("/Users/arangill/Desktop/ftp_conn.obj"):
+        if os.path.getsize("/Users/arangill/Desktop/ftp_conn.obj") > 0:      
+            open_conn = open('/Users/arangill/Desktop/ftp_conn.obj', 'rb') 
+            conn = pickle.load(open_conn)
 
-def download_file(filename):
+            #Connects to FTP using creds from pickle
+            connect_ftp(conn.hostname[0], conn.username[0], conn.password[0])
+            #Stores creds for next time
+            keep_conn(conn)
+
+            return True
+
+    #If file does not exist
+    return False
+
+def download_file(date, time):
+    #reformats date and time to match csv files
+    final = date + " " + time
+    final = datetime.datetime.strptime(final, "%d-%m-%Y %H:%M:%S").strftime("%Y%m%d%H%M%S")
+    filename = "MED_DATA_" + final + ".csv"
+
+    #Downloads File    
     with open(filename, "wb") as f:
         ftp.retrbinary("RETR " + filename, f.write)
         print("Successfully downloaded " + filename)
-
-def print_directory():
-    return ftp.pwd()
     
+    
+def main():
+        global args
+        parser = argparse.ArgumentParser(description="A CLI FTP Client")
 
-def change_directory(new_directory):
-    ftp.cwd(new_directory)
+        parser.add_argument("-hn", "--hostname", 
+                            type = str, 
+                            nargs = 1,  
+                            metavar = "hostname", 
+                            help = "Hostname of FTP server")
 
-    print("Changed to " + print_directory())
+        parser.add_argument("-u", "--username", 
+                            type = str, 
+                            nargs = 1,  
+                            metavar = "username", 
+                            help = "Username to connect to FTP")
+
+        parser.add_argument("-p,", "--password", 
+                            type = str, 
+                            nargs = 1,  
+                            metavar = "password", 
+                            help = "Password to connect to FTP")
+
+        parser.add_argument("-s,", "--schedule", 
+                            type = str, 
+                            nargs = 2,  
+                            metavar = "date_time", 
+                            help = "Specify the date and time to download the file as DD-MM-YYYY HH:MM:SS")
 
 
+        args = parser.parse_args()
 
-connect_ftp(hostname, username, password)
+        #Checks if pickle exists from previous session
+        if conn_exists():
+            pass
 
-while True:
-    user_input = input("\n" + username + "@" + hostname + "> ")
-
-    try:
-        command = user_input.split()[0]
-        try:
-            parameter = user_input.split()[1]
-            
-            if command == "download":
-                download_file(parameter)
-
-            elif command == "cd":
-                change_directory(parameter)
-         
-        except:
-            print()
-            
-
-        if command == "pwd":
-            print(print_directory())
-
-        elif command == "ls":
-            list_files()
- 
-        elif command == "exit":
-            ftp.quit()
-            print("Server disconnected")
-            print()
-            break
-
+        #If user is starting a new session
+        if args.hostname != None and args.username != None and args.password != None:
+            connect_ftp(args.hostname[0], args.username[0], args.password[0])
         
-       
-        
-    except:
-        print("Please provide a command")
+        #If user has entered data for schedule
+        if args.schedule != None:
+            download_file(args.schedule[0], args.schedule[1])
+
+
+if __name__ == "__main__":
+    main()
