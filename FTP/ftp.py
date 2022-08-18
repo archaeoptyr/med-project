dbfffd14bd1c25e5a49647d07a3979e05daabbb6
Rskas <110549485+Rskas@users.noreply.github.com>
2022-08-18 23:58:17 +0100

    Added Cron functionality for mac
    -Full file access must be enabled for cron to work on mac

--- FTP/ftp.py
+++ FTP/ftp.py
@@ -1,10 +1,9 @@
 import argparse
-from genericpath import isfile
 import pickle
 from ftplib import FTP
-import time
 import os
 from datetime import datetime
+from subprocess import Popen
 
 ftp = None
 
@@ -22,10 +21,13 @@ def connect_ftp(hostname, username, password):
     
     except:
         print("Unable to connect")
-        print("\nClosing in 5 seconds")
-        time.sleep(5)
         quit()
-    
+
+def rm_conn():
+    #if pickle object exists
+    if os.path.isfile("ftp_conn.obj"):
+        #delete pickle file
+        os.remove("ftp_conn.obj")
 
 def keep_conn(cred):
     #creates pickle file to store ftp creds
@@ -50,18 +52,22 @@ def conn_exists():
     return False
 
 def format_datetime(date, time):
+    #formats date and time
     date_time = date + " " + time
-    date_time = datetime.strptime(date_time, "%d-%m-%Y %H:%M:%S").strftime("%Y%m%d%H%M%S")
+    date_time = datetime.strptime(date_time, "%d-%m-%Y %H:%M:%S")
     return date_time
 
 def format_date(date):
+    #Only formats the date
     format_date = datetime.strptime(date, "%Y-%m-%d").strftime("%Y%m%d")
     return format_date
 
 def download_file(date, time):
     #reformats date and time to match csv files
     date_time = format_datetime(date, time)
-    filename = "MED_DATA_" + date_time + ".csv"
+    date_time = date_time.strftime("%Y%m%d%H%M%S")
+    filename = "MED_DATA_" + str(date_time) + ".csv"
+    print(filename)
 
     #Downloads File    
     with open(filename, "wb") as f:
@@ -69,65 +75,133 @@ def download_file(date, time):
         print("Successfully downloaded " + filename)
     
 def download_file_default(date_now):
+    #downloads files from the current day if specific date is not specified
     date = format_date(date_now)
-    print(date)
+    file_list = list_files()
+    file_needed = "MED_DATA_" + date
+
+    for name in file_list:
+        #if the file in the server matches today's date
+        if file_needed in name:  
+            with open(name, "wb") as f:
+                ftp.retrbinary("RETR " + name, f.write)
+                print("Successfully downloaded " + name)
+
+
+def list_files():
+    #returns an array of files in the server
+    list_of_files = ftp.nlst()
+
+    return list_of_files
+
+def mac_osx(date, time):
+    date_time = format_datetime(date, time)
+
+    #Applescript to create a cron
+    create_cron = ('''tell application "Terminal"
+                    if not (exists window 1) then reopen
+                    activate
+                    tell application "System Events" to keystroke "crontab -e"
+                    tell application "System Events" to keystroke return
+                    tell application "System Events" to keystroke "i"
+                    tell application "System Events" to keystroke "{minute} {hour} {day} {month} * cd /Users/arangill/Desktop/med-project/FTP && /usr/bin/python schedule.py"
+                    tell application "System Events" to keystroke (key code 53)
+                    tell application "System Events" to keystroke ";" using shift down
+                    tell application "System Events" to keystroke "wq"
+                    tell application "System Events" to keystroke return
+                    delay 10
+                    quit
+                    end tell'''.format(minute = date_time.strftime("%M"),
+                                        hour = date_time.strftime("%H"),
+                                        day = date_time.strftime("%d"),
+                                        month = date_time.strftime("%m")))
+    #execute the applescript
+    p = Popen(['osascript', '-e', create_cron])
+
     
+
+
 def main():
         global args
+        #Description of the CLI
         parser = argparse.ArgumentParser(description="A CLI FTP Client")
 
+        #Specify the hostname of the server
         parser.add_argument("-hn", "--hostname", 
                             type = str, 
                             nargs = 1,  
                             metavar = "hostname", 
                             help = "Hostname of FTP server")
 
+        #Specify the username to access server
         parser.add_argument("-u", "--username", 
                             type = str, 
                             nargs = 1,  
                             metavar = "username", 
                             help = "Username to connect to FTP")
 
-        parser.add_argument("-p,", "--password", 
+        #Specify the password to access server
+        parser.add_argument("-p", "--password", 
                             type = str, 
                             nargs = 1,  
                             metavar = "password", 
                             help = "Password to connect to FTP")
 
-        parser.add_argument("-s,", "--schedule", 
+        #Schedule a download for a specific date and time
+        parser.add_argument("-s", "--schedule", 
                             type = str, 
                             nargs = "*",  
                             metavar = "date", 
                             help = "Specify the date and time to download the file as DD-MM-YYYY HH:MM:SS")
-        """
-        parser.add_argument("-s,", "--schedule", 
+        
+        #Download the specified file
+        parser.add_argument("-d", "--download", 
                             type = str, 
-                            nargs = '',  
-                            metavar = "date_time", 
-                            help = "Specify the date and time to download the file as DD-MM-YYYY HH:MM:SS")
-        """
+                            nargs = "*",  
+                            metavar = "date", 
+                            help = "Specify the date and time of the file you wish to download")
+        
+        #Disconnect from the server (i.e. delete credentials) 
+        parser.add_argument("-dc", "--disconnect", 
+                            type = str, 
+                            nargs = '*',  
+                            metavar = "disconnect", 
+                            help = "Disconnect from FTP server")
+         
 
         args = parser.parse_args()
-
-        #Checks if pickle exists from previous session
-        if conn_exists():
-            pass
-
-        #If user is starting a new session
-        if args.hostname != None and args.username != None and args.password != None:
-            connect_ftp(args.hostname[0], args.username[0], args.password[0])
         
-        #If user has entered data for schedule
-        if args.schedule != None and len(args.schedule) == 2:
-            download_file(args.schedule[0], args.schedule[1])
+        try:
+            if args.disconnect != None:
+                rm_conn()
+                print("Disconnected")
 
-        elif args.schedule != None and len(args.schedule) == 0:
-            current_date_time = str(datetime.now())
-            current_date_time = current_date_time[:10]
+            #Checks if pickle exists from previous session
+            if conn_exists():
+                pass
 
-            download_file_default(current_date_time)
+            #If user is starting a new session
+            if args.hostname != None and args.username != None and args.password != None:
+                connect_ftp(args.hostname[0], args.username[0], args.password[0])
             
+            #If user has entered data for schedule
+            if args.download != None and len(args.download) == 2:
+                download_file(args.download[0], args.download[1])
+            
+            if args.schedule != None and len(args.schedule) == 0:
+                current_date_time = str(datetime.now())
+                current_date_time = current_date_time[:10]
 
+                download_file_default(current_date_time)
+            
+            if args.schedule != None and len(args.schedule) == 2:
+                mac_osx(args.schedule[0], args.schedule[1])
+                
+            
+        
+        except AttributeError:
+            print("Please specify your credentials")
+        
 
 if __name__ == "__main__":
     main()
---
+++ FTP/schedule.py
@@ -0,0 +1,3 @@
+f = open('test123.txt', 'w')
+f.write("THIS WORKED")
+f.close()
